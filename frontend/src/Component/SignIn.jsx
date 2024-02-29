12191c84f17ee1d321825390ad0285990a5283d8
Adarsh tripathi <adarshtripathi2070@gmail.com>
2024-02-29 12:20:48 +0530

    myCommit

--- frontend/src/Component/SignIn.jsx
+++ frontend/src/Component/SignIn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useHistory } from "react-router-dom";
-import { toast } from "react-toastify"
+ 
 function SignIn() {
   const history = useHistory();
   const [input, setInput] = useState({
